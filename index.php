21e9e9f52deb06ef5afa9b170935f7004397f853
mirlanAligator <miraligat@gmail.com>
2015-10-26 12:51:46 +0600

    add
    add new file

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Mirlan
+ * Date: 26.10.15
+ * Time: 12:38
+ * To change this template use File | Settings | File Templates.
+ */
+echo "Hello!";
+ 
